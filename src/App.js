763ddc5bb7547149de18d8074ce30dc4d526cb64
Mehmet Yener YILMAZ <yilmazmehmetyener@gmail.com>
2023-03-07 14:41:34 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
+import { WebMapView } from "./maps/WebMapView";
+import { BaseMaps } from "./maps/BaseMaps";
+import { DrawMapView } from "./maps/Draw";
+import { Editor } from "./maps/Editor";
 
-function App() {
+export default function App() {
+  const [Mapitems] = useState([
+    <WebMapView />,
+    <BaseMaps />,
+    <DrawMapView />,
+    <Editor />
+  ]);
+
+  const [items] = useState([
+    { label: "Simple Map View", value: 0 },
+    { label: "Base Map Gallery", value: 1 },
+    { label: "Draw Map", value: 2 },
+    { label: "Editor Map", value: 3 }
+  ]);
+
+  const [value, setValue] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ height: "3vh" }}>
+        <b>React ESRI Map Examples --> </b>
+        <select onChange={e => setValue(e.currentTarget.value)}>
+          {items.map(item => (
+            <option key={item.value} value={item.value}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+      </div>
+      {Mapitems[value]}
+    </>
   );
 }
-
-export default App;
